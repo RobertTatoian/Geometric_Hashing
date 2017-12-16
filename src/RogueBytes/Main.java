793299ac32d645e68a02c8a123dac5caf6f081c4
Robert Tatoian <RobertTatoian@users.noreply.github.com>
2017-12-16 11:57:39 -0500

    Everything with the model appears to be working, corrected issue with angle not being correct if the point was below the X axis. Another indexing issue remains...
    Signed-off-by: Robert Tatoian <RobertTatoian@users.noreply.github.com>

--- src/RogueBytes/Main.java
+++ src/RogueBytes/Main.java
@@ -9,20 +9,6 @@ import java.util.Map;
 public class Main extends PApplet implements VisualizationConstants{
 
     private int[][] orionImage = {{2262, 2029},{2648, 1743},{2870, 2119},{2862, 2205},{2844, 2289},{3116, 2715},{3420, 2279}};
-    private int[][][] model = {
-            // Orion
-            {{381, 381}, {924, 573}, {690, 1047}, {600, 1098}, {504, 1134}, {249, 1683}, {891, 1707}},
-            // Auriga
-            {{119,181}, {149,231}, {228,255}, {244,201}, {189,145}, {163,144}}
-    };
-
-    private Point[][] model_Points = {
-            {new Point(381, 381), new Point(924, 573), new Point(690, 1047), new Point(600, 1098),
-                    new Point(504, 1134) ,new Point(249, 1683) ,new Point(891, 1707)},
-
-            {new Point(119,181), new Point(149,231), new Point(228,255), new Point(244,201),
-                    new Point(189,145) ,new Point(163,144)}
-    };
 
     private Model[] m = {
             new Model(0, new Point[] {new Point(381, 381), new Point(924, 573), new Point(690, 1047), new Point(600, 1098),
@@ -34,14 +20,10 @@ public class Main extends PApplet implements VisualizationConstants{
 
     Map<String, List<int[]>> possibleModels = new HashMap<String, List<int[]>>();
 
-    private int[][] workingModel;
-    private float[][] scaledModel;
-    private float[][] scaledShiftedModel;
-    private float[][] ssrModel;
-
-    Point[] sP;
-    Point[] ssP;
-    Point[] ssrP;
+    private Point[] workingModel;
+    private Point[] scaledModel;
+    private Point[] scaledShiftedModel;
+    private Point[] ssrModel;
 
     private float scale = 1;
     private double distance = 0;
@@ -73,91 +55,6 @@ public class Main extends PApplet implements VisualizationConstants{
     }
 
     /**
-     *  Calculates the distance between two points.
-     *
-     *  @param x1 The x coordinate of the first point.
-     *  @param x2 The x coordinate of the second point.
-     *  @param y1 The y coordinate of the first point.
-     *  @param y2 The y coordinate of the second point.
-     *  @return The distance between the two points.
-     */
-    private double distance(float x1, float x2, float y1, float y2){
-        float a = ((x2 - x1) * (x2 - x1));
-        float b = ((y2 - y1) * (y2 - y1));
-        float sum = a + b;
-        return Math.sqrt(sum);
-    }
-
-    /**
-     * Scales all the points of a model by a certain factor.
-     * @param coordinates The list of coordinates to scale.
-     * @param scale The amount to scale by.
-     * @return The list of scaled coordinates.
-     */
-    private float[][] scaleCoordinates(int[][] coordinates, float scale) {
-        float[][] scaledCoordinates = new float[coordinates.length][coordinates[0].length];
-
-        for (int i = 0; i < coordinates.length; i++) {
-            for (int j = 0; j < coordinates[0].length; j++) {
-                scaledCoordinates[i][j] = coordinates[i][j] * scale;
-            }
-        }
-
-        return scaledCoordinates;
-    }
-
-    /**
-     * Shifts all the points of a model by adding a shift amount the provided coordinates.
-     * @param coordinates The list of coordinates to shift.
-     * @param xShift The amount to shift by in the x axis.
-     * @param yShift The amount to shift by in the y axis.
-     * @return The list of shifted coordinates.
-     */
-    private float[][] shiftCoordinates(float[][] coordinates, float xShift, float yShift) {
-        float[][] shiftedCoordinates = new float[coordinates.length][coordinates[0].length];
-
-        for (int i = 0; i < coordinates.length; i++) {
-            shiftedCoordinates[i][0] = coordinates[i][0] + xShift;
-            shiftedCoordinates[i][1] = coordinates[i][1] + yShift;
-        }
-
-        return shiftedCoordinates;
-    }
-
-    /**
-     * Shifts all the points of a model by a provided angle.
-     * @param coordinates The list of coordinates to rotate.
-     * @param angle The angle to rotate by.
-     * @return The list of rotated coordinates.
-     */
-    private float[][] rotateCoordinates(float[][] coordinates, float angle) {
-        float[][] rotatedCoordinates = new float[coordinates.length][coordinates[0].length];
-
-        for (int i = 0; i < coordinates.length; i++) {
-            rotatedCoordinates[i] = rotatePoint(coordinates[i][0], coordinates[i][1], angle);
-        }
-
-        return rotatedCoordinates;
-    }
-
-    /**
-     * Calculates the rotation of a point about the origin.
-     *
-     * @param x The x coordinate of the point.
-     * @param y The y coordinate of the point.
-     * @param angle The angle to rotate by.
-     * @return An array containing the new coordinates of the point.
-     */
-    private float[] rotatePoint(float x, float y, float angle) {
-        float[] rotatedCoordinates = {0, 0};
-
-        rotatedCoordinates[0] = x * cos(angle) + y * sin(angle);
-        rotatedCoordinates[1] = -(x * sin(angle)) + y * cos(angle);
-
-        return rotatedCoordinates;
-    }
-
-    /**
      * Calculates the angle between the adjacent and hypotenuse.
      * @param adjacent The length of the adjacent side of the triangle.
      * @param hypotenuse The length of the hypotenuse of the triangle.
@@ -166,46 +63,6 @@ public class Main extends PApplet implements VisualizationConstants{
     private double calculateAngle(float adjacent, float hypotenuse){
 
         return Math.acos(adjacent/hypotenuse);
-    }
-
-
-    private void drawModelPoints(float[][] coordinates) {
-
-        int numberOfPoints = coordinates.length;
-
-        for (int j = 0; j < numberOfPoints; j++) {
-            // For the life of me I can't remember how to change the color depending on with iteration
-            // the loop is on and this part is not important in the slightest, so...
-            switch (j){
-                case 0:
-                    stroke(255, 0, 0);
-                    break;
-                case 1:
-                    stroke(0, 255, 0);
-                    break;
-                case 2:
-                    stroke(0, 0, 255);
-                    break;
-                case 3:
-                    stroke(255, 0, 255);
-                    break;
-                case 4:
-                    stroke(255, 255, 0);
-                    break;
-                case 5:
-                    stroke(0, 255, 255);
-                    break;
-                case 6:
-                    stroke(255, 255, 255);
-                    break;
-                default:
-                    stroke(0);
-                    break;
-            }
-            point(coordinates[j][0], coordinates[j][1]);
-        }
-
-        stroke(0, 0, 0);
     }
 
     private void drawModelPoints(Point[] coordinates) {
@@ -252,71 +109,61 @@ public class Main extends PApplet implements VisualizationConstants{
     }
 
     public void draw() {
-//        background(100f);
-
+        background(100f);
+        String model = Model.generateKey(currentModel, i, shift);
+        text(model, 0, 100);
         setupVisualization();
 
-        workingModel = model[currentModel];
-
-//        if (shift != i) {
-//            //Calculate the distance between two points and determine the value to scale it to 1.
-//            distance = distance(workingModel[i][0], workingModel[shift][0], workingModel[i][1], workingModel[shift][1]);
-//            scale = (float) (1 / distance);
-//
-//            //Scale all the points such that the distance between the two points calculated above is 1.
-//            scaledModel = scaleCoordinates(workingModel, scale);
-//
-//            //Shift all the points such that the current point we are working on is at (0, 0).
-//            scaledShiftedModel = shiftCoordinates(scaledModel, -scaledModel[i][0], -scaledModel[i][1]);
-//
-//            // Recalculate the scaled distance
-//            distance = distance(scaledShiftedModel[i][0], scaledShiftedModel[shift][0], scaledShiftedModel[i][1], scaledShiftedModel[shift][1]);
-//
-//            // Calulate the angle between the second point and the x-axis.
-//            angle = (float) calculateAngle(scaledShiftedModel[shift][0], (float) distance);
-//
-//            // Rotate the model by the calculated angle.
-//            ssrModel = rotateCoordinates(scaledShiftedModel, angle);
-//
-//            //TODO Implement hash table storage
-//
-//            println(generateKey(currentModel, i, shift));
-//
-//        }
-
-        Point[] working = m[currentModel].getModelPoints();
+        workingModel = m[currentModel].getModelPoints();
 
         if (shift != i) {
             //Calculate the distance between two points and determine the value to scale it to 1.
-            distance = Point.distance(working[i],working[shift]);
+            distance = Point.distance(workingModel[i],workingModel[shift]);
             scale = (float) (1 / distance);
 
-            sP = Model.scaleCoordinates(working, scale);
+            //Scale all the points such that the distance between the two points calculated above is 1.
+            scaledModel = Model.scaleCoordinates(workingModel, scale);
 
-            ssP = Model.shiftCoordinates(sP, working[i]);
+            //Shift all the points such that the current point we are working on is at (0, 0).
+            scaledShiftedModel = Model.shiftCoordinates(scaledModel, scaledModel[i]);
 
-            distance = Point.distance(ssP[i],ssP[shift]);
+            // Recalculate the scaled distance
+            distance = Point.distance(scaledShiftedModel[i],scaledShiftedModel[shift]);
+            println("The distance for model: " + model + " is: " + (float) distance);
+            println("The adjacent side for our model: " + model + " is: " + (float) scaledShiftedModel[shift].getX());
 
-            angle = (float) calculateAngle((float) ssP[shift].getX(), (float) distance);
+            // Calulate the angle between the second point and the x-axis.
+            angle = (float) calculateAngle((float) scaledShiftedModel[shift].getX(), (float) distance);
+            println("The angle calculated to rotate our model: " + model + " is: " + angle);
 
-            ssrP = Model.rotateCoordinates(ssP, angle);
+            // If the Y of the point in below the x axis, invert the angle.
+            if (scaledShiftedModel[shift].getY() < 0) {
+                angle = -angle;
+            }
+            
+            // Rotate the model by the calculated angle.
+            ssrModel = Model.rotateCoordinates(scaledShiftedModel, angle);
+
+            //TODO Implement hash table storage
+
+//            println(Model.generateKey(currentModel, i, shift));
         }
 
-        drawModelPoints(ssrP);
         drawModelPoints(ssrModel);
 
+
+    }
+
+    public void mouseClicked() {
         if (i == workingModel.length - 1) {
             i = 0;
-             currentModel = ((currentModel + 1) % model.length);
+            currentModel = ((currentModel + 1) % m[currentModel].getModelPoints().length);
         } else if (shift == workingModel.length - 1){
             i++;
         }
 
         shift = ((shift + 1) % workingModel.length);
-
-        delay(1000);
     }
-
 
 
     public static void main(String[] args) {
